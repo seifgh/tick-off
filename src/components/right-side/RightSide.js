42167a9052a89c3df95415675d2c60c702e0fad3
Seif Gharres <seifgarrese2@gmail.com>
2020-08-04 15:07:45 +0100

    improving the code

--- src/components/right-side/RightSide.js
+++ src/components/right-side/RightSide.js
@@ -9,44 +9,30 @@ import CompletedTasks from './CompletedTasks.js';
 
 function RightSide(props) {
 
-	//react hooks
 	const { getActiveList, deleteListTasks } = useContext(GlobalContext);
-
-	const [state, setState] = useState({
-		show_completed_tasks: true,
-	})
-
-	function toggleCompletedTasks() {
-		setState({
-			show_completed_tasks: !state.show_completed_tasks
-		})
-	}
+	const [showCompletedTasks, setShowCompletedTasks] = useState(false);
 
 	const list = getActiveList();
 	if (list) {
 		const { id, title, tasks } = list;
 		const completed_tasks = tasks.filter(task => task.is_completed);
 		const incompleted_tasks = tasks.filter(task => !task.is_completed);
-		const { show_completed_tasks } = state;
 
 		return (
 			<div className="right-side">
 				<TopBar
-					show_completed_tasks={show_completed_tasks}
-					toggleCompletedTasks={toggleCompletedTasks}
+					showCompletedTasks={showCompletedTasks}
+					toggleCompletedTasks={() => setShowCompletedTasks(!showCompletedTasks)}
 					deleteAllTasks={() => deleteListTasks(id)}
 					title={title}
 				/>
 				<Tasks list_id={id} tasks={incompleted_tasks} />
 				<AddTask list_id={id} />
-				<CompletedTasks list_id={id} show={show_completed_tasks} tasks={completed_tasks} />
-
+				<CompletedTasks list_id={id} show={showCompletedTasks} tasks={completed_tasks} />
 			</div>
 		)
 	}
-	return (
-		<div></div>
-	)
+	return null
 }
 
 export default RightSide;
